ed11fa0fd208191544847c2363d33036ceee19c0
MiradoAndrian04 <miradoandrian25@gmail.com>
2024-09-24 14:21:37 +0300

    js

--- Admin/Profil/Profil.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("openFormBtn").addEventListener("click", function() {
-    document.getElementById("popupForm").style.display = "flex";
-});
-
-document.getElementById("closeFormBtn").addEventListener("click", function() {
-    document.getElementById("popupForm").style.display = "none";
-});
